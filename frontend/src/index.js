c63ceeec6215d1d0d5c51a220684551e1c7a5506
Tanisha <tanishasuhag127@gmail.com>
2025-08-18 23:45:42 +0530

    fixed issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -34,7 +34,6 @@ root.render(
         <Route path="/partners" element={<Partners />} />
         <Route path="/carbon" element={<Carbon />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
